b5c5fd71918fc0e7ad3c10a6b8b6274587740ae6
Pedro Henrique <165854227+phpedruo@users.noreply.github.com>
2026-01-19 16:04:59 -0300

    Update phone number in enviarWhats function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function enviarWhats(event) {
     event.preventDefault()
     let nome = document.getElementById('nome').value;
     let mensagem = document.getElementById('mensagem').value;
-    let telefone = '5581999999999';
+    let telefone = '5581998456922';
 
     let texto = `Olá! me chamo ${nome}, ${mensagem}`;
     let msgFormatada = encodeURIComponent(texto)
